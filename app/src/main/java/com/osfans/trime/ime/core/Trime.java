e771cb1fb319680b12d28d610176f60768cf157d
wxyzh <wxyzhen@live.com>
2023-02-26 09:58:07 +0800

    roll-back updatecomposing to 3.2.7

--- app/src/main/java/com/osfans/trime/ime/core/Trime.java
+++ app/src/main/java/com/osfans/trime/ime/core/Trime.java
@@ -1205,11 +1205,9 @@ public class Trime extends LifecycleInputMethodService {
         Timber.d("updateComposing() SymbolKeyboardType=%s", symbolKeyboardType.toString());
         if (symbolKeyboardType != SymbolKeyboardType.NO_KEY
             && symbolKeyboardType != SymbolKeyboardType.CANDIDATE) {
-          mComposition.setWindow();
-          showCompositionView(false);
-          return 0;
+            mComposition.getRootView().setVisibility(View.GONE);
         } else {
-
+            mComposition.getRootView().setVisibility(View.VISIBLE);
           startNum = mComposition.setWindow(minPopupSize, minPopupCheckSize, Integer.MAX_VALUE);
           mCandidate.setText(startNum);
           // if isCursorUpdated, showCompositionView will be called in onUpdateCursorAnchorInfo
@@ -1226,6 +1224,14 @@ public class Trime extends LifecycleInputMethodService {
     if (mainKeyboardView != null) mainKeyboardView.invalidateComposingKeys();
     if (!onEvaluateInputViewShown())
       setCandidatesViewShown(textInputManager.isComposable()); // 實體鍵盤打字時顯示候選欄
+     if (symbolKeyboardType == SymbolKeyboardType.CANDIDATE) {
+      if (isComposing()) {
+        liquidKeyboard.updateCandidates();
+      } else {
+        selectLiquidKeyboard(-1);
+      }
+    }  
+    
 
     return startNum;
   }
